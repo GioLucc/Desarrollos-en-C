b1cd91c9cff864966c655e2f89b27f9e327b037c
Giovanni <giolucarna@gmail.com>
2021-11-11 11:11:00 -0300

    :tada: Initial late commit

---
+++ TP_[3]/BasicFunctions.h
@@ -0,0 +1,27 @@
+/*
+ * BasicFunctions.h
+ *
+ *  Created on: 19 oct. 2021
+ *      Author: giova
+ */
+
+#ifndef BASICFUNCTIONS_H_
+#define BASICFUNCTIONS_H_
+#define MAX 100
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void printMenu(void);
+int getValidInt(char mensaje [], char error [], int min, int max);
+int OnlyIntegrersValidation(char integrer[], char mensaje[],char error [], int min, int max);
+int getString(char mensaje[],char str[]);
+int OnlyCharacterValidation(char str[]);
+int askForAdress (char message [], char str[]);
+int getStringVerificado(char mensaje[], char mensajeError[], char *input);
+int argentinianCuitValidation (char mensaje[], char mensajeError[], char *input);
+
+
+#endif /* BASICFUNCTIONS_H_ */
---
+++ TP_[3]/parser.h
@@ -0,0 +1,15 @@
+/*
+ * parser.h
+ *
+ *  Created on: 3 nov. 2021
+ *      Author: giova
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
+int parser_EmployeeToSaveAsText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+#endif /* PARSER_H_ */
